3ff2afa8322bbcaf603b1382e0ffcd60b4063878
Vanessa Garcia <112432406+VanessaGVilla@users.noreply.github.com>
2023-02-10 15:06:51 -0800

    For each commit

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace COMP003A.Assignment4
+﻿/*Author: Vanessa Garcia
+ * Course: Comp - 003A - L01
+ * Purpose: Assignment 4 Looping Statements
+*/
+
+
+namespace COMP003A.Assignment4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("******************************************");
+            Console.WriteLine("Friends");
+            Console.WriteLine("Array");
         }
     }
 }
